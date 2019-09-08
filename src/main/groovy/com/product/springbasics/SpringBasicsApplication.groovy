e2664613541cc5e56881080dffe22da220d3a136
Zachary Lockner <zacharylockner@zacharys-mbp.lan>
2019-09-08 14:37:20 -0500

    Initializing...

---
+++ src/main/groovy/com/product/springbasics/SpringBasicsApplication.groovy
@@ -0,0 +1,13 @@
+package com.product.springbasics
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBasicsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringBasicsApplication, args)
+	}
+
+}
